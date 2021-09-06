c93e540cf00373dc4d55e04fb5064571c5702fa9
g-hoost <gfroehling@icloud.com>
2021-09-06 15:44:34 +0200

    introduction multiple items

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App()
   return (
     <div className="App">
       <h1>Let's get started!</h1>
-      <p>This is also visible.</p>
+      <ExpenseItem />
+      <ExpenseItem />
+      <ExpenseItem />
       <ExpenseItem />
     </div>
 
