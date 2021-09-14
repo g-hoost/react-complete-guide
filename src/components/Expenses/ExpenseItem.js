0a95f92ef0eba9814bad326923b55057dec77a4b
g-hoost <gfroehling@icloud.com>
2021-09-14 09:56:03 +0200

    first event handler

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,14 +4,22 @@ import Card from '../UI/Card';
 
 function ExpenseItem(props)
 {
+    let title = props.title;
+
+    const clickHandler = () =>
+    {
+        title = 'Updated!';
+        console.log(title);
+    }
 
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">€{props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     )
 };
