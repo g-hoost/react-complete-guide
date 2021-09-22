34290adc2f432b6fb0e64367273e64b08ad82e82
g-hoost <gfroehling@icloud.com>
2021-09-22 11:01:04 +0200

    conditional content done

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import ExpenseItem from '../Expenses/ExpenseItem';
 import '../Expenses/ExpenseItem.css';
 import Card from '../UI/Card';
 import '../Expenses/Expenses.css';
 import ExpensesFilter from '../Expenses/ExpensesFilter';
 import './ExpensesFilter.css';
+import ExpensesList from '../Expenses/ExpensesList'
 
 
 function Expenses(props)
@@ -21,36 +21,13 @@ function Expenses(props)
         return expense.date.getFullYear().toString() === filteredYear;
     });
 
-    let expensesContent = <p className='no-expenses'>No Expenses found.</p>;
-    if (filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.map((expense) => (
-            <ExpenseItem
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}
-            />
-        ))
-    }
-
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter
                     selected={filteredYear}
                     onChangeFilter={filterChangeHandler} />
-                {expensesContent
-                /* {filteredExpenses.length === 0 && expensesContent}
-                {filteredExpenses.length > 0 &&
-                    filteredExpenses.map((expense) => (
-                        <ExpenseItem
-                            key={expense.id}
-                            title={expense.title}
-                            amount={expense.amount}
-                            date={expense.date}
-                        />
-                        hier wäre dann "no expenses found" der Default-Wert!
-                    ))} */}
+                <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
     )
